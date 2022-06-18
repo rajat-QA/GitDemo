01f1e9220dbf8ef81255a4d6fb72001f28172b52
rajat-QA <rajatmahadik2@gmail.com>
2022-06-18 20:42:34 +0530

    American guy commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -10,6 +10,13 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		//dd
+		//adad
+		//rer
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
 	}
 	
 	@Test
